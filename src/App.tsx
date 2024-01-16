add03e81c14571f955302bac46899481740c9ddc
Thomaz Alves <thomazhilario5@gmail.com>
2024-01-16 13:47:02 -0300

    Criando a socialTree

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import Information from "./Components/Information"
-
+import SocialTree from "./Components/SocialTree"
 function App() {
   return (
     <main id="container">
+      {/* Componente Information */}
       <Information/>
+
+      {/* Componente SocialTree */}
+      <SocialTree/>
     </main>
   )
 }
---
+++ src/Components/SocialTree/index.tsx
@@ -0,0 +1,12 @@
+import './socialtree.css'
+
+export default function SocialTree(){
+    return(
+        <ul>
+            <li><a>GitHub</a></li>
+            <li><a>Youtube</a></li>
+            <li><a>Twitter</a></li>
+            <li><a>Instagram</a></li>
+        </ul>
+    )
+}
